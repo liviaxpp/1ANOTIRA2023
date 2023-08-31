e6bc7c34f1df1a34a685737f33390ddd5696c03e
liviaxpp <143632127+liviaxpp@users.noreply.github.com>
2023-08-31 12:39:20 -0300

    Add files via upload

---
+++ Jogo_do_Pong_2023_08_31_15_30_04/sketch.js
@@ -0,0 +1,51 @@
+//variáveis da bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 15;
+let raio = diametro / 2;
+
+//velocidade da bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
+
+//variaveis da bolinha
+let xRaquete = 5;
+let yRaquete = 150;
+let raqueteComprimento = 10;
+let raqueteAltura = 90;
+    
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  mostraBolinha()
+  movimentaBolinha()
+  verificaColisaoBorda()
+  mostraRaquete()
+  movimentaMinhaRaquete
+}
+
+function mostraBolinha(){
+circle(xBolinha, yBolinha, diametro)
+}
+
+function movimentaBolinha(){
+  xBolinha += velocidadeXBolinha;
+  yBolinha += velocidadeYBolinha;
+}
+
+function verificaColisaoBorda(){
+if (xBolinha + raio > width || 
+    xBolinha - raio < 0){
+ velocidadeXBolinha *= -1;
+  }
+if (yBolinha + raio > height || 
+    yBolinha - raio < 0){
+ velocidadeYBolinha *= -1;
+  }
+}
+  
+function mostraRaquete(){
